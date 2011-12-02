26689b4784b5d4581d9c3df21e942b41660d4acb
tbranyen <tim@tabdeveloper.com>
2011-12-01 23:19:28 -0500

    updated code sample with comments from @visionmedia

--- posts/backbone-collections-node-rss/assets/delivery.js
+++ posts/backbone-collections-node-rss/assets/delivery.js
@@ -1,4 +1,4 @@
 site.get("/rss.xml", function(req, res) {
-  res.writeHead(200, { "Content-Type": "application/rss+xml" });
-  res.end(posts.feed.xml());
+  res.contentType("rss");
+  res.send(posts.feed.xml());
 });
