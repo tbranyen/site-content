f70ffaac5bb17230f2116cd0c24e1704a4fa5a25
tbranyen <tim@tabdeveloper.com>
2012-01-16 18:51:10 -0500

    added note about using noConflict in the source

--- posts/require_use_shim/assets/my-example.js
+++ posts/require_use_shim/assets/my-example.js
@@ -4,6 +4,9 @@ require.config({
     backbone: {
       deps: ["use!underscore", "jquery"],
       attach: function() {
+        // If you plan on loading any plugins for Backbone, do not use this
+        // approach of removing it from the global scope.  They will be
+        // unable to find Backbone.
         return window.Backbone.noConflict();
       }
     },
